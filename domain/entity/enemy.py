9d0e3ecb64851f837a8ad2eda46ad1084b9cc4ef
KarelVrabik <Vrabik.Karel@unicorncollege.cz>
2025-06-15 10:05:01 +0200

    add astar

--- domain/entity/enemy.py
+++ domain/entity/enemy.py
@@ -1,6 +1,12 @@
+import traceback
+
 import pygame
 import math
 import random
+
+import domain.entity.bomb
+from utils.astar import AStar
+
 
 class Enemy(pygame.sprite.Sprite):
     def __init__(self, x, y, iso_utils):
@@ -35,34 +41,62 @@ class Enemy(pygame.sprite.Sprite):
         if self.move_timer > 10:
             self.move_timer = 0
             bomb_proximity = 4
+            explosion_proximity = 1
+            danger_proximity = 4
             dx = 0
             dy = 0
 
-            # pronásledování hráče
-            if random.random() < 0.5:
-                if self.grid_x - player.grid_x > 0:
-                    dx = -1
-                else:
-                    dx = 1
-            else:
-                if self.grid_y - player.grid_y > 0:
-                    dy = -1
-                else:
-                    dy = 1
 
-            for danger in dangers:
-                # jestli utíkat
-                if abs(self.grid_x - danger.grid_x) < bomb_proximity and abs(self.grid_y - danger.grid_y) < bomb_proximity:
-                    # kudy utíkat
-                    if self.grid_x - danger.grid_x > 0:
-                        dx = 1
-                    else:
+            # pronásledování hráče
+            try:
+                astar=AStar(game_map)
+                path=astar.find_path((self.grid_x,self.grid_y),(player.grid_x,player.grid_y))
+            except :
+                traceback.print_exc()
+                path=[]
+            if len(path)>1:
+                print(path[1],self.grid_x,self.grid_y)
+                print(path[1][0],path[1][1])
+                dx=path[1][0]-self.grid_x
+                dy=path[1][1]-self.grid_y
+            else:#není li cesta
+                if random.random() < 0.5:
+                    if self.grid_x - player.grid_x > 0:
                         dx = -1
-                    if self.grid_y - danger.grid_y > 0:
-                        dy = 1
                     else:
+                        dx = 1
+                else:
+                    if self.grid_y - player.grid_y > 0:
                         dy = -1
-                    break
+                    else:
+                        dy = 1
+
+
+
+
+            for danger in dangers:#TODO seřadit nebezpečí od nejbližšího
+                if type(danger) == domain.entity.bomb.Bomb:
+                    danger_proximity=bomb_proximity
+                if type(danger) == domain.entity.explosion.Explosion:
+                    danger_proximity=explosion_proximity
+
+                # jestli utíkat
+                d_x_dist=self.grid_x - danger.grid_x
+                d_y_dist=self.grid_y - danger.grid_y
+                if abs(d_x_dist) < danger_proximity and abs(d_y_dist) < danger_proximity:
+                    # kudy utíkat
+                    if abs(d_x_dist) > abs(d_y_dist):
+                        if d_x_dist > 0:
+                            dx = 1
+                        else:
+                            dx = -1
+                        break
+                    else:
+                        if d_y_dist > 0:
+                            dy = 1
+                        else:
+                            dy = -1
+                        break
 
             new_x = self.grid_x + dx
             new_y = self.grid_y + dy
